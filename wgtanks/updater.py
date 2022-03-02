bcb8c6da13196b23bcc797c2fa4c4e184f841e08
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-02 13:33:23 +0900

    Update update.py(激ウマギャグ)
    classと__init__の追加、とりあえずフォルダの作成機能だけ

---
+++ wgtanks/updater.py
@@ -0,0 +1,19 @@
+import os
+import json
+import requests
+
+# Constantinopolis
+CDN_URL = "https://raw.githubusercontent.com/TiberSe/wgapi-cdn/main/"
+TANK_LIST = "https://raw.githubusercontent.com/TiberSe/wgapi-cdn/main/wot/tanks/list.json"
+
+
+class WotApiUpdater:
+
+    def __init__(self, folder):
+        self.current_dir = folder
+        self.data_folder = f"{self.current_dir}{os.sep}wotapi_data"
+        if not os.path.exists(self.data_folder):
+            os.mkdir(self.data_folder)
+
+        self.ships_file = f"{self.data_folder}{os.sep}tanks.json"
+        self.files = [self.ships_file]

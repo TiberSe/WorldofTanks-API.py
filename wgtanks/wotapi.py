afe37691ccaebbe1e3653e77e5db7c68f281b2e5
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-02 18:17:45 +0900

    Update wotapi.py
    minimum

---
+++ wgtanks/wotapi.py
@@ -0,0 +1,13 @@
+import os
+import json
+import requests
+
+from .updater import WotApiUpdater
+
+
+class WoTAPI:
+
+    def __init__(self, folder=os.getcwd()):
+        self.updater = WotApiUpdater(folder)
+        self.updater.update()
+

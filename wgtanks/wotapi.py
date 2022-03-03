791f0cbd63469ace210ce8c7891535146f14622a
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-03 21:00:56 +0900

    Update wotapi.py

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -24,12 +24,28 @@ class WoTAPI:
         self._updater = WotApiUpdater(folder)
         self._updater.update()
 
-    def get_account_by_id(self, account_id: int, extra: str = '', fields: tuple = ()) -> dict:
+    def get_account_info_by_id(self, account_id: int, extra: str = '', fields: tuple = ()) -> dict:
         if not isinstance(account_id, int):
-            raise IllegalTypeException(account_id)
-            return False
-        fields = re.sub("\(|\'|\)|\ ", '', str(fields))
+            raise IllegalTypeException(var_name='account_id', contents=account_id, intend='Integer')
+        fields = self.parse_tuple(fields)
         response = requests.get(f'{self._ACCOUNT_URL}/info/?application_id={self._API_TOKEN}&account_id={account_id}&'
                                 f'&fields={fields}&extra={extra}&language={self._API_LANG}')
         account_data = json.loads(response.text)
         return account_data
+
+    def get_account_id_by_name(self, account_name: str, limit: int = 5, exact: bool = True, fields: tuple = ()) -> dict:
+        if not isinstance(account_name, str):
+            raise IllegalTypeException(var_name='account_name', value=account_name, intend='longer than 3 chars')
+        if not isinstance(limit, int):
+            raise IllegalTypeException(var_name='limit', value=limit, intend='Integer')
+        fields = self.parse_tuple(fields)
+        exact = 'exact' if exact else 'startswith'
+        response = requests.get(f'{self._ACCOUNT_URL}/list/?application_id={self._API_TOKEN}&search={account_name}&'
+                                f'type={exact}&fields={fields}&language={self._API_LANG}')
+        account_data = json.loads(response.text)
+        return account_data
+
+    @staticmethod
+    def parse_tuple(data: tuple) -> str:
+        data = re.sub("\(|\'|\)|\ ", '', str(data))
+        return data

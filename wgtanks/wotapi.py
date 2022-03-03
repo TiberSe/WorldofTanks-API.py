be377f9b66c01069f0abf8c76954741f5ea4cdeb
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-03 16:32:32 +0900

    Update wotapi.py
    insert utilities import
    and append definition for constant in __init__

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -2,13 +2,21 @@ import os
 import json
 import requests
 
+from utils.enum import Region
+from utils.exception import *
 from .updater import WotApiUpdater
 
 
 class WoTAPI:
 
-    def __init__(self, folder=os.getcwd()):
+    def __init__(self, api_token: str, region=Region.ASIA, folder=os.getcwd()):
+        # Definition Constant
+        self.API_TOKEN = api_token
+        self.REGION = region
+        self.MAIN_URL = region.value
+        self.ACCOUNT_URL = f"{self.MAIN_URL}/account"
+        self.CLANS_URL = f"{self.MAIN_URL}/clans"
+        self.GMAP_URL = f"{self.MAIN_URL}/globalmap"
+        # Initialize API Updater
         self.updater = WotApiUpdater(folder)
         self.updater.update()
-        
-

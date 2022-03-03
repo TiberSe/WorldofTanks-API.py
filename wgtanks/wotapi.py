ced808737f4352ccb135b28b7765f9e213cdad94
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-03 19:38:42 +0900

    Update wotapi.py

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import re
 
 from .utils.enum import Region
 from .utils.exception import *
@@ -9,11 +10,12 @@ from .updater import WotApiUpdater
 
 class WoTAPI:
 
-    def __init__(self, api_token: str, region=Region.ASIA, lang='ja', folder=os.getcwd()):
+    def __init__(self, api_token: str, region=Region.ASIA, lang='en', folder=os.getcwd()):
         # Definition Constant
         self._API_TOKEN = api_token
         self._REGION = region
-        self._LANG = lang
+        self._LOCAL_LANG = lang
+        self._API_LANG = 'en'  # TODO: 'en'はru地域ではサポートされてないからregionがruのときだけ言語もruにするコードをいつか書く。
         self._MAIN_URL = region.value
         self._ACCOUNT_URL = f'{self._MAIN_URL}/account'
         self._CLANS_URL = f'{self._MAIN_URL}/clans'
@@ -22,10 +24,12 @@ class WoTAPI:
         self._updater = WotApiUpdater(folder)
         self._updater.update()
 
-    def get_account_by_id(self, account_id: int, **kwargs) -> dict:
+    def get_account_by_id(self, account_id: int, extra: str = '', fields: tuple = ()) -> dict:
         if not isinstance(account_id, int):
             raise IllegalTypeException(account_id)
             return False
-        response = requests.get(f'{self._ACCOUNT_URL}/info/?application_id={self._API_TOKEN}&account_id={account_id}')
+        fields = re.sub("\(|\'|\)|\ ", '', str(fields))
+        response = requests.get(f'{self._ACCOUNT_URL}/info/?application_id={self._API_TOKEN}&account_id={account_id}&'
+                                f'&fields={fields}&extra={extra}&language={self._API_LANG}')
         account_data = json.loads(response.text)
         return account_data

82b63de28412a9d2c27fd5456d84e3e58b5f738f
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-03 17:19:27 +0900

    Update wotapi.py
    change constant to private

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -2,21 +2,31 @@ import os
 import json
 import requests
 
-from utils.enum import Region
-from utils.exception import *
+from .utils.enum import Region
+from .utils.exception import *
 from .updater import WotApiUpdater
 
 
 class WoTAPI:
 
-    def __init__(self, api_token: str, region=Region.ASIA, folder=os.getcwd()):
+    def __init__(self, api_token: str, region=Region.ASIA, lang="ja", folder=os.getcwd()):
         # Definition Constant
-        self.API_TOKEN = api_token
-        self.REGION = region
-        self.MAIN_URL = region.value
-        self.ACCOUNT_URL = f"{self.MAIN_URL}/account"
-        self.CLANS_URL = f"{self.MAIN_URL}/clans"
-        self.GMAP_URL = f"{self.MAIN_URL}/globalmap"
+        self._API_TOKEN = api_token
+        self._REGION = region
+        self._LANG = lang
+        self._MAIN_URL = region.value
+        self._ACCOUNT_URL = f"{self._MAIN_URL}/account"
+        self._CLANS_URL = f"{self._MAIN_URL}/clans"
+        self._GMAP_URL = f"{self._MAIN_URL}/globalmap"
         # Initialize API Updater
-        self.updater = WotApiUpdater(folder)
-        self.updater.update()
+        self._updater = WotApiUpdater(folder)
+        self._updater.update()
+
+    def get_account_by_id(self, account_id: int, **kwargs) -> dict:
+        if not isinstance(account_id, int):
+            raise IllegalTypeException(account_id)
+            return False
+        request_url = f"{self._ACCOUNT_URL}/info/?application_id={self._API_TOKEN}"
+        response = requests.get(f'{self._ACCOUNT_URL}/info/?application_id={self._API_TOKEN}&account_id={account_id}')
+        account_data = json.loads(response.text)
+        return account_data

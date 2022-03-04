e5479f1744aec383581784d09c2a913e585beefa
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-04 11:45:21 +0900

    Update wotapi.py
    add docstrings

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -2,6 +2,7 @@ import os
 import json
 import requests
 import re
+import time
 
 from .utils.enum import Region
 from .utils.exception import *
@@ -25,6 +26,12 @@ class WoTAPI:
         self._updater.update()
 
     def get_account_info_by_id(self, account_id: int, extra: str = '', fields: tuple = ()) -> dict:
+        """Retrieves the account info by account ID.
+        :param int account_id:
+        :param str extra:
+        :param tuple fields:
+        :return:
+        """
         if not isinstance(account_id, int):
             raise IllegalTypeException(var_name='account_id', contents=account_id, intend='Integer')
         fields = self.parse_tuple(fields)
@@ -34,11 +41,32 @@ class WoTAPI:
         return account_data
 
     def get_account_id_by_name(self, account_name: str, limit: int = 5, exact: bool = True, fields: tuple = ()) -> dict:
+        """Find and retrieves the account ID by account name.
+
+        :param str account_name: the name of the account to lookup. (Min: 3 chars)
+        :param int limit: the maximum number of results to return. (Max: 100)
+        :param bool exact: whether to return only results that match the exact string (Default: True)
+        :param tuple fields: a tuple of fields to return in the results
+
+        :return: dict containing the account ID
+        """
+        __params__ = [
+            {
+                'account_name': account_name,
+                'type': str,
+                'minimum': 3
+            }
+        ]
+        if not len(account_name) >= 3:
+            raise IllegalLengthException(var_name='account_name', value=account_name, intend='longer than 3 chars')
         if not isinstance(account_name, str):
-            raise IllegalTypeException(var_name='account_name', value=account_name, intend='longer than 3 chars')
+            IllegalTypeException(var_name='account_name', value=account_name, intend='String')
+        if not isinstance(fields, tuple):
+            IllegalTypeException(var_name='fields', value=fields, intend='Tuple')
         if not isinstance(limit, int):
             raise IllegalTypeException(var_name='limit', value=limit, intend='Integer')
-        fields = self.parse_tuple(fields)
+
+        fields = self.__parse_tuple(fields)
         exact = 'exact' if exact else 'startswith'
         response = requests.get(f'{self._ACCOUNT_URL}/list/?application_id={self._API_TOKEN}&search={account_name}&'
                                 f'type={exact}&fields={fields}&language={self._API_LANG}')
@@ -46,6 +74,12 @@ class WoTAPI:
         return account_data
 
     @staticmethod
-    def parse_tuple(data: tuple) -> str:
+    def __parse_tuple(data: tuple) -> str:
         data = re.sub("\(|\'|\)|\ ", '', str(data))
         return data
+
+
+    def __integrity_check(self, data: list):
+
+        return
+

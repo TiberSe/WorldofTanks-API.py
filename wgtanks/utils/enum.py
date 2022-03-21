95a154f6fee59d08c48057f9dd5637a3905a6a05
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-21 17:31:41 +0900

    Update enum.py

--- wgtanks/utils/enum.py
+++ wgtanks/utils/enum.py
@@ -3,3 +3,12 @@ from enum import Enum
 
 class Region(Enum):
     ASIA = 'https://api.worldoftanks.asia/wot'
+    NA = 'https://api.worldoftanks.com/wot'
+    EU = 'https://api.worldoftanks.eu/wot'
+    RU = 'https://api.worldoftanks.ru/wot'
+
+
+class Lang(Enum):
+    EN = 'en'
+    RU = 'ru'
+    TH = 'th'

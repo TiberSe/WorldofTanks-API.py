2d88b95b03733c7f2cb28b58055c0faa77ddbc47
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-03-25 16:03:18 +0900

    Update wotapi.py

--- wgtanks/wotapi.py
+++ wgtanks/wotapi.py
@@ -206,14 +206,22 @@ class WoTAPI:
         return_data = json.loads(response.text)
         return return_data
 
-    def get_dates_with_available_ratings(self, limit: int = 7):
+    def get_dates_with_available_clan_ratings(self, limit: int = 7):
         """Retrieves types of ratings.
 
         :param int limit: The int of Limit to the dates with available ratings to lookup. (Max: 365)
 
         :return: dict containing the player's clan history
         """
-
+        args = locals()
+        args = self.__fix_params(args)
+        self.__integrity_check(args)
+        args = self.__parse_tuple(args)
+        url = f'{self._CLANRATINGS_URL}/dates/?application_id={self._API_TOKEN}' \
+              '&limit={limit}'.format(**args)
+        response = requests.get(url)
+        return_data = json.loads(response.text)
+        return return_data
 
     def __fix_params(self, args: dict):
         for param in self._params[inspect.stack()[1].function]:
